73252a900bff2ecaa4496f078b5dbca60d843ddf
Jason Carman <jasonmcarman@gmail.com>
2024-10-13 22:49:41 -0400

    Initial commit.

---
+++ backup.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: Jason Carman; jasonmcarman@gmail.com
+# Date: October 13, 2024
+# Purpose: Backup my Ubuntu VMs for OSL645 and OSL745
+# Usage: ./backup.bash
+#
+
+# Check if the user is root
