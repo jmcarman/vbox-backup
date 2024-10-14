50b54004c8259a76f19bac2c3393a4d9fdf0f10d
Jason Carman <jasonmcarman@gmail.com>
2024-10-13 22:57:35 -0400

    Added initial version of backup script.

--- backup.bash
+++ backup.bash
@@ -6,3 +6,22 @@
 #
 
 # Check if the user is root
+if [[ $(whoami) != "root" ]]; then
+    echo "You must be root to run this script."
+    exit 1
+fi
+
+# Check if the source exits
+if [[ ! -d /media/candice/KINGSTON/OSL645/Ubuntu-jmcarman/ ]]; then
+    echo "The source directory does not exist."
+    exit 2
+fi
+
+# Check if the destination exists
+if [[ ! -d /mnt/FA4E05CC4E05831D/Documents/Jason\ Documents/Backups ]]; then
+    echo "The destination directory does not exist."
+    exit 3
+fi
+
+# Copy the source to the destination
+cp -r /media/candice/KINGSTON/OSL645/Ubuntu-jmcarman/ /mnt/FA4E05CC4E05831D/Documents/Jason\ Documents/Backups/
